2fb2feeb879e6d87ef83dbf718d7a772f497f4a6
youssefmechichi <youssef.mechichi@esprit.tn>
2026-07-05 01:47:24 +0200

    configure Azure remote backend

--- infra/terraform/environments/bootstrap/main.tf
+++ infra/terraform/environments/bootstrap/main.tf
@@ -1,14 +1,14 @@
 module "resource_group" {
-  source = "../../modules/resource-group"
+  source   = "../../modules/resource-group"
   name     = "cloudcost-bootstrap-rg"
   location = "France Central"
 }
 
 module "terraform_backend" {
-  source = "../../modules/terraform-backend"
-  resource_group_name = module.resource_group.name
-  location            = module.resource_group.location
+  source               = "../../modules/terraform-backend"
+  resource_group_name  = module.resource_group.name
+  location             = module.resource_group.location
   storage_account_name = "cloudcostyousseftfstate"
-  container_name = "tfstate"
+  container_name       = "tfstate"
 }
 
