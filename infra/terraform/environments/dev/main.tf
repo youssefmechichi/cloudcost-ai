b08c5000e04a3bb8c456491331f90ced91ab4875
youssefmechichi <youssef.mechichi@esprit.tn>
2026-07-05 03:48:29 +0200

    implement reusable Azure network module

---
+++ infra/terraform/environments/dev/main.tf
@@ -0,0 +1,15 @@
+data "azurerm_resource_group" "bootstrap" {
+  name = "cloudcost-bootstrap-rg"
+}
+
+module "network" {
+  source              = "../../modules/network"
+  resource_group_name = data.azurerm_resource_group.bootstrap.name
+  location            = data.azurerm_resource_group.bootstrap.location
+  vnet_name           = local.vnet_name
+  address_space       = local.address_space
+  subnet_prefixes     = local.subnet_prefixes
+}
+
+
+
