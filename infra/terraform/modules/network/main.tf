85643b36b18c8d1e339a4ad1e66a7b13c155f9fe
youssefmechichi <youssef.mechichi@esprit.tn>
2026-07-08 22:45:17 +0200

    fix: allow HTTP/HTTPS traffic to AKS ingress through subnet NSG

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -41,6 +41,33 @@ resource "azurerm_network_security_group" "this" {
   resource_group_name = var.resource_group_name
 }
 
+# Allow public HTTP/HTTPS traffic to reach the AKS ingress controller.
+# Azure Load Balancer health probes are allowed by default, but client
+# traffic from the Internet must also be permitted on the AKS subnet NSG.
+
+resource "azurerm_network_security_rule" "allow_http_https" {
+  for_each = {
+    for name, subnet in var.subnets :
+    name => subnet
+    if name == "aks"
+  }
+
+  name                        = "allow-http-https"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+
+  source_port_range           = "*"
+  destination_port_ranges     = ["80", "443"]
+
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this[each.key].name
+}
+
 resource "azurerm_subnet_network_security_group_association" "this" {
 
     for_each = var.subnets
